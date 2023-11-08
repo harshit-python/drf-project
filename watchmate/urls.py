75e52502f6c417fa306551274ab5651d26c86d63
harsh <hsaxena1998@gmail.com>
2023-11-09 00:35:17 +0530

    adding temporary authentication

--- watchmate/urls.py
+++ watchmate/urls.py
@@ -7,4 +7,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("watchlist_app.api.urls")),
+    path("api-auth/", include("rest_framework.urls")),
 ]
